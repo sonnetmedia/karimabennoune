6dca2885b4209de72dd79572de6337cbd3314dce
Bud Parr <budparr@sonnetmedia.net>
2014-06-13 11:49:52 -0400

    remove unused script references

--- src/assets/js/concatenate-scripts.js
+++ src/assets/js/concatenate-scripts.js
@@ -1,7 +1,2 @@
 ---
 ---
-{% include scripts/jquery.min.js %}
-{% include scripts/widow-control.js %}
-{% include scripts/accordian.js %}
-{% include scripts/nav-top-mobile.js %}
-{% include scripts/smooth-scroll.js %}
